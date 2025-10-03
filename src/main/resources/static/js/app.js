d85fb72e7ae5cc94c232117dc9c5ebed334c57e1
dadc1007 <dadica1007@gmail.com>
2025-10-02 20:59:42 -0500

    alerta si el usuario no existe y muestra el nombre del autor en el titulo de la tabla

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,41 +1,49 @@
 var app = (function () {
-    var selectedAuthor = null;
-    var blueprints = [];
+  var selectedAuthor = null;
+  var blueprints = [];
 
-    function mapBlueprints(list) {
-        return list.map(function (bp) {
-            return {
-                name: bp.name,
-                points: bp.points.length,
-            };
-        });
-    }
+  function mapBlueprints(list) {
+    return list.map(function (bp) {
+      return {
+        name: bp.name,
+        points: bp.points.length,
+      };
+    });
+  }
 
-    return {
-        setAuthor: function (author) {
-            selectedAuthor = author;
-        },
-        getAuthor: function () {
-            return selectedAuthor;
-        },
-        loadBlueprints: function (author) {
-            apimock.getBlueprintsByAuthor(author, function (data) {
-                blueprints = mapBlueprints(data);
-                $("table tbody").empty();
-                blueprints.map(function (bp) {
-                    $("table tbody").append(
-                        `<tr>
+  return {
+    setAuthor: function (author) {
+      selectedAuthor = author;
+    },
+    getAuthor: function () {
+      return selectedAuthor;
+    },
+    loadBlueprints: function (author) {
+      apimock.getBlueprintsByAuthor(author, function (data) {
+        if (!data || data.length == 0) {
+          alert("Author not found");
+          return;
+        }
+        const authorBlueprintsElement =
+          document.getElementById("author-blueprints");
+        authorBlueprintsElement.textContent = `${author}'s blueprints:`;
+        blueprints = mapBlueprints(data);
+        $("table tbody").empty();
+        blueprints.map(function (bp) {
+          $("table tbody").append(
+            `<tr>
                             <td>${bp.name}</td>
                             <td>${bp.points}</td>
-                            <td><button type="button" class="btn btn-primary btn-sm">Open</button></td>
+                            <td></td>
                         </tr>`
-                    );
-                });
-                var total = blueprints.reduce(function (acc, bp) {
-                    return acc + bp.points;
-                }, 0);
-                $("p").text("Total user points: " + total);
-            });
-        },
-    };
+          );
+        });
+        var total = blueprints.reduce(function (acc, bp) {
+          return acc + bp.points;
+        }, 0);
+        const totalPointsElement = document.getElementById("total-points");
+        totalPointsElement.textContent = `Total user points: ${total}`;
+      });
+    },
+  };
 })();
